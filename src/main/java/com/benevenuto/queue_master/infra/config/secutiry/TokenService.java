50941aece0b304c8dcda328e9e4ad0a03c2592e8
Gustavo Benevenuto <gustavo.benesilva@hotmail.com>
2026-06-20 18:10:17 +1000

    Ajustes

--- src/main/java/com/benevenuto/queue_master/infra/config/secutiry/TokenService.java
+++ src/main/java/com/benevenuto/queue_master/infra/config/secutiry/TokenService.java
@@ -15,18 +15,21 @@ import com.benevenuto.queue_master.domain.user.entity.User;
 
 @Component
 public class TokenService {
-	@Value("${api.security.token.secret}")
-	private String secret;
+    @Value("${api.security.token.secret}")
+    private String secret;
 
     public String generateToken(User user) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
 
             String token = JWT.create()
-					.withIssuer("queue-master") // Define quem emitiu o token
-					.withSubject(user.getEmail()) // Define o assunto do token, aqui o e-mail do usuário
-					.withExpiresAt(generateExpirationDate()) // Define a data de expiração do token
-					.sign(algorithm); // Assina o token com o algoritmo definido
+                    .withIssuer("queue-master") // Define quem emitiu o token
+                    .withSubject(user.getEmail()) // Define o assunto do token, aqui o e-mail do usuário
+                    .withClaim("id", String.valueOf(user.getId()))
+                    .withClaim("name", user.getName())
+                    .withClaim("role", user.getRole().name())
+                    .withExpiresAt(generateExpirationDate()) // Define a data de expiração do token
+                    .sign(algorithm); // Assina o token com o algoritmo definido
             return token;
         } catch (JWTCreationException ex) {
             throw new RuntimeException("Error while generating token", ex);
@@ -35,14 +38,14 @@ public class TokenService {
 
     public String validateToken(String token) {
         try {
-        	// Define novamente o algoritmo HMAC256 com o mesmo segredo
-			Algorithm algorithm = Algorithm.HMAC256(secret);
-			
-			return JWT.require(algorithm)
-					.withIssuer("queue-master") // Confirma que o token foi emitido por "SpringAuth"
-					.build()
-					.verify(token) // Verifica a validade do token
-					.getSubject(); // Retorna o assunto do token, que é o e-mail do usuário
+            // Define novamente o algoritmo HMAC256 com o mesmo segredo
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer("queue-master") // Confirma que o token foi emitido por "SpringAuth"
+                    .build()
+                    .verify(token) // Verifica a validade do token
+                    .getSubject(); // Retorna o assunto do token, que é o e-mail do usuário
         } catch (JWTVerificationException ex) {
             return ""; // Token inválido
         }
